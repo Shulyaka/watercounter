ec1655320c18649c69ffc1ac127a3ba7bda408b9
Denis Shulyaka <Shulyaka@gmail.com>
2016-04-08 23:32:43 +0300

    save and read state

--- main.c
+++ main.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <err.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <signal.h>
 #include <time.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define SIG_GPIO_IRQ	42
 
 #define GPIO_CHIP	0
-#define NUMCOUNTER	3
+#define MAXCOUNTER	20
 
 enum state
 {
 	lo=0,
-	hi=1
+	hi=1,
+	unknown
 };
 
 struct counter
@@ -22,7 +27,11 @@ struct counter
 	unsigned long value;
 	enum state laststate;
 	time_t timestamp;
-} counter[NUMCOUNTER]={{0}, {1}, {24}};
+	char filename[256];
+} counter[MAXCOUNTER];
+
+int numcounter=0;
+int gpio_base;
 
 void counter_update(int gpio, enum state val)
 {
@@ -30,21 +39,22 @@ void counter_update(int gpio, enum state val)
 
 	printf("gpio: %d, state: %d\n", gpio, val);
 
-	for(i=0; i<NUMCOUNTER && counter[i].gpio!=gpio; i++);
+	for(i=0; i<numcounter && counter[i].gpio!=gpio; i++);
 
-	if(i==NUMCOUNTER)
+	if(i==numcounter)
 	{
 		printf("Warning: Unknown gpio pin %d\n", gpio);
 		return;
 	}
 
 	if(val==hi)
+	{
 		counter[i].value++;
+		printf("counter: %d, value: %lu\n", i, counter[i].value);
+	}
 
 	counter[i].timestamp=time(NULL);
 	counter[i].laststate=val;
-
-	printf("counter: %d, value: %lu\n", i, counter[i].value);
 }
 
 void irq_handler(int n, siginfo_t *info, void *unused)
@@ -56,7 +66,6 @@ void gpio_init(void)
 {
 	FILE *file;
 	char tmpstr[256];
-	int gpio_base;
 	int i;
 	struct sigaction sig;
 
@@ -111,7 +120,7 @@ void gpio_init(void)
 	if(!(file=fopen("/sys/kernel/debug/gpio-irq", "w")))
 		err(1, "Unable to open /sys/kernel/debug/gpio-irq");
 
-	for(i=0; i<NUMCOUNTER; i++)
+	for(i=0; i<numcounter; i++)
 	{
 		counter[i].gpio+=gpio_base;
 		if(fprintf(file, "+ %d %d\n", counter[i].gpio, getpid()) <= 0)
@@ -134,7 +143,7 @@ void gpio_free(void)
 	if(!(file=fopen("/sys/kernel/debug/gpio-irq", "w")))
 		err(1, "Unable to open /sys/kernel/debug/gpio-irq");
 
-	for(i=0; i<NUMCOUNTER; i++)
+	for(i=0; i<numcounter; i++)
 	{
 		if(fprintf(file, "- %d %d\n", counter[i].gpio, getpid()) <= 0)
 			err(1, "Unable to clear irq handler for gpio %d", counter[i].gpio);
@@ -163,10 +172,90 @@ void gpio_free(void)
 */
 }
 
+int namefilter(const struct dirent *de)
+{
+	if(strncmp(de->d_name, "counter", 7))
+		return 0;
+	return de->d_name[7]>='0' && de->d_name[7]<='9' && (de->d_type==DT_LNK || de->d_type==DT_REG || de->d_type==DT_UNKNOWN);
+}
+
+void counter_init(const char *dirp)
+{
+	struct dirent **namelist;
+	struct stat filestat;
+	FILE *file;
+	int i;
+
+	if((numcounter=scandir(dirp, &namelist, namefilter, alphasort))<0)
+		err(1, "scandir %s", dirp);
+
+	if(numcounter==0 || numcounter > MAXCOUNTER)
+	{
+		printf("Invalid counter count\n");
+		exit(1);
+	}
+
+	for(i=0; i<numcounter; i++)
+	{
+		sprintf(counter[i].filename, "%s/%s", dirp, namelist[i]->d_name);
+
+		counter[i].gpio=atoi(namelist[i]->d_name+7);
+
+		if(stat(counter[i].filename, &filestat)<0)
+			err(1, "stat %s", counter[i].filename);
+
+		counter[i].timestamp=filestat.st_mtim.tv_sec;
+
+		counter[i].laststate=unknown;
+
+		if(!(file=fopen(counter[i].filename, "r")))
+			err(1, "open %s", counter[i].filename);
+
+		if(fscanf(file, "%lu", &counter[i].value)!=1)
+			err(1, "read %s", counter[i].filename);
+
+		fclose(file);
+
+		free(namelist[i]);
+	}
+
+	free(namelist);
+}
+
+void counter_free(void)
+{
+	FILE *file;
+	int i;
+
+	for(i=0; i<numcounter; i++)
+	{
+		if(!(file=fopen(counter[i].filename, "w")))
+			err(1, "open %s", counter[i].filename);
+
+		if(fprintf(file, "%lu", counter[i].value)<=0)
+			err(1, "write %s", counter[i].filename);
+
+		fclose(file);
+	}
+}
+
+void counter_print(void)
+{
+	int i;
+
+	printf("Current counters:\n");
+
+	for(i=0; i<numcounter; i++)
+		printf("Number: %d, gpio: %d, value: %lu, state: %d, timestamp: %ld, filename: %s\n", i, counter[i].gpio, counter[i].value, counter[i].laststate, counter[i].timestamp, counter[i].filename);
+}
+
 int main(void)
 {
 	sigset_t sigset;
 	siginfo_t siginfo;
+
+	counter_init("/root/watercounters");
+	counter_print();
 
 	gpio_init();
 
@@ -185,6 +274,9 @@ int main(void)
 	}
 
 	gpio_free();
+
+	counter_print();
+	counter_free();
 
 	printf("\nNormal exit\n");
 
