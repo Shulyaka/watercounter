80b087437425bd02d945d811befb4908b90f06ad
Denis Shulyaka <Shulyaka@gmail.com>
2017-11-26 01:02:16 +0300

    Web interface, example cli inteface commands and example configuration

---
+++ www/cgi-bin/wcontrol.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+echo "Status: 200 OK"
+echo "Content-Type: application/json"
+echo "Cache-Control: no-cache"
+echo "Expires: 0"
+echo ""
+
+QUERY_STRING="${QUERY_STRING//&/ }"
+
+for ARG in $QUERY_STRING; do
+	PAR="${ARG%%=*}"  #What the f#*=k is %%=*?
+	VAL="${ARG#*=}"
+	case "$PAR" in
+		valve)
+			VALVE="$VAL"
+			;;
+		function)
+			FUNCTION="$VAL"
+			;;
+		_)
+			;;
+		*)
+			echo "{\"success\":0,\"error\":\"Unknown parameter $PAR\"}"
+			exit
+			;;
+	esac
+done
+
+case "$FUNCTION" in
+	open|close) ;;
+	*)
+		echo "{\"success\":0,\"error\":\"Unknown function\"}"
+		exit
+	;;
+esac
+
+for COUNTER in /etc/watercounter/counter*; do
+	COUNTER="$(basename "$COUNTER")"
+	test "${COUNTER%%-opkg}" == "$COUNTER" || continue
+	COUNTER="${COUNTER#counter*_}"
+
+	NAME="${COUNTER%_*}"
+	test "$NAME" == "$VALVE" || continue
+
+	$VALVE $FUNCTION
+
+	echo "{\"success\":1}"
+	exit
+done
+
+echo "{\"success\":0,\"error\":\"Valve not found\"}"
---
+++ www/cgi-bin/wcounter.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo "Status: 200 OK"
+echo "Content-Type: application/json"
+echo "Cache-Control: no-cache"
+echo "Expires: 0"
+echo ""
+echo -n "{\"counters\":["
+NOTFIRST=""
+for COUNTER in /etc/watercounter/counter*; do
+	COUNTER="$(basename "$COUNTER")"
+	test "${COUNTER%%-opkg}" == "$COUNTER" || continue
+	test -n "$NOTFIRST" && echo -n ","
+	NOTFIRST=Y
+	COUNTER="${COUNTER#counter*_}"
+
+	NAME="${COUNTER%_*}"
+	SERIAL="${COUNTER##*_}"
+	VALUE="$(counter $COUNTER counter)"
+	STATE="$(${COUNTER%_*} state)"
+
+	echo -n "{\"name\":\"$NAME\","
+	echo -n "\"serial\":\"$SERIAL\","
+	echo -n "\"value\":${VALUE:-null},"
+	echo -n "\"state\":${STATE:-null}}"
+done
+echo "]}"
+
